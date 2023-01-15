8295bb3d2c7c8a497e63631580bf5acc830b1881
Andrew McGoveran <andrewmcgoveran@gmail.com>
2023-01-15 18:10:16 -0500

    Show game filters instead of hiding them behind a button

--- app/components/GameFilter/GameFilter.tsx
+++ app/components/GameFilter/GameFilter.tsx
@@ -28,59 +28,35 @@ function getWeekOptions(season: string) {
 }
 
 export function GameFilter() {
-  const transition = useTransition();
   const submit = useSubmit();
   const [searchParams] = useSearchParams();
 
-  const [filterOpen, setFilterOpen] = React.useState(false);
   const initialSeason = searchParams.get('season') || defaultSeason;
   const initialWeek = searchParams.get('week') || defaultWeek;
   const [currentSeason, setCurrentSeason] = React.useState(initialSeason);
   const weekOptions = getWeekOptions(currentSeason);
-  const submitting = Boolean(transition.submission);
-
-  if (!filterOpen) {
-    return (
-      <div className="GameFilter--toggle">
-        <button className="secondary" onClick={() => setFilterOpen(true)}>
-          Filter games
-        </button>
-      </div>
-    );
-  }
 
   function handleFormChange(event: React.ChangeEvent<HTMLFormElement>) {
     submit(event.currentTarget, {replace: true});
   }
 
   return (
-    <div className="card">
-      <Form method="get" className="GameFilter" onChange={handleFormChange}>
-        <div className="GameFilter--Filters">
-          <Select
-            label="Select season"
-            name="season"
-            options={seasonOptions}
-            initialValue={initialSeason}
-            onChange={setCurrentSeason}
-          />
-          <Select
-            label="Select week"
-            name="week"
-            options={weekOptions}
-            initialValue={initialWeek}
-          />
-        </div>
-        <div className="GameFilter--buttons">
-          <button
-            onClick={() => setFilterOpen(false)}
-            className="secondary"
-            disabled={submitting}
-          >
-            Cancel
-          </button>
-        </div>
-      </Form>
-    </div>
+    <Form method="get" className="GameFilter" onChange={handleFormChange}>
+      <Select
+        label="Select week"
+        labelHidden
+        name="week"
+        options={weekOptions}
+        initialValue={initialWeek}
+      />
+      <Select
+        label="Select season"
+        labelHidden
+        name="season"
+        options={seasonOptions}
+        initialValue={initialSeason}
+        onChange={setCurrentSeason}
+      />
+    </Form>
   );
 }
