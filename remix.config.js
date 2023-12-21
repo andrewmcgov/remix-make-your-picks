48195a324b094f5121f39534fd0f4d8b66d53338
Andrew McGoveran <andrewmcgoveran@gmail.com>
2023-12-21 15:55:32 -0500

    Enable new server build path and maintain module format as cjs for now

--- remix.config.js
+++ remix.config.js
@@ -5,8 +5,15 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildDirectory: 'api/_build',
   ignoredRouteFiles: ['.*'],
+  // Values for vercel build https://remix.run/docs/en/1.19.3/pages/v2#vercel
+  publicPath: '/build/', // default value, can be removed
+  serverBuildPath: 'api/index.js',
+  serverMainFields: ['main', 'module'], // default value, can be removed
+  serverMinify: false, // default value, can be removed
+  serverModuleFormat: 'cjs', // default value, can be removed
+  serverPlatform: 'node', // default value, can be removed
+  // ----------------------------------
   future: {
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
